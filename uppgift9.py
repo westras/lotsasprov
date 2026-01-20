5f89edc99394c63e1fb21a857aa33cdc564c0554
westras <r.westholm@telia.com>
2026-01-20 11:00:09 +0100

    ww

---
+++ uppgift9.py
@@ -0,0 +1,28 @@
+#Uppgift 9 – Polymorfism (8 p)
+
+#Skapa ett exempel som visar polymorfism:
+
+#· minst en basklass
+
+#· minst två subklasser
+
+#· samma metodnamn i alla klasser
+
+#· visa (med kod) att rätt metod anropas beroende på objektets typ
+
+class Djur:
+    def djurläte(self):
+        print("...")
+        
+class Hund(Djur):
+    def djurläte(self):
+        print("voff")
+        
+class Katt(Djur):
+    def djurläte(self):
+        print("Mjau")
+        
+lista = [Hund(), Katt()]
+
+for f in lista:
+    f.djurläte() 
