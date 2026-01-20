59a75f61267b4ffcdc8b72f00a65910e20222623
westras <r.westholm@telia.com>
2026-01-20 10:40:36 +0100

    t

---
+++ uppgift8.py
@@ -0,0 +1,24 @@
+#Uppgift 8 – Arv (6 p)
+
+#Skapa:
+
+#· en basklass Fordon med metoden kora()
+
+#· en subklass Bil som ärver från Fordon
+
+#· Bil ska skriva ut något mer specifikt än basklassen när kora() anropas
+
+class Fordon:
+    def kora():
+        return "..."
+    
+class Bil(Fordon):
+    def kora():
+        return "brum BrumBRUM"
+    
+f = Fordon
+b = Bil
+
+print(f.kora)
+print(b.kora)
+    
