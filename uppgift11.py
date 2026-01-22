ca415786db7ae92e8a7df924e869383afb3003bf
westras <r.westholm@telia.com>
2026-01-22 10:51:43 +0100

    a

---
+++ uppgift11.py
@@ -0,0 +1,14 @@
+#Uppgift 11 – Arv med gemensam basklass (6 p)
+
+#Skapa:
+
+#· en basklass Anstalld med attribut namn
+
+#· en metod beskrivning() som skriver ut något generellt
+
+#· två subklasser: Larare och Rektor
+
+#· båda subklasserna ska skriva över beskrivning()
+
+#Visa gärna ett exempel på hur metoden anropas.
+
