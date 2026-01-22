008c55551bbb913e6e8b64d4dab8c1a869f66cf5
westras <r.westholm@telia.com>
2026-01-22 11:02:22 +0100

    b

--- uppgift11.py
+++ uppgift11.py
@@ -12,3 +12,26 @@
 
 #Visa gärna ett exempel på hur metoden anropas.
 
+class Anställd:
+    def __init__(self, namn):
+        self.namn = namn
+        
+    def beskrivning(self):
+        print("Information")
+    
+class Lärare(Anställd):
+    def __init__(self, namn):
+        super().__init__(namn)
+    
+    def beskrivning(self):
+        return super().beskrivning()
+    
+class Rektor(Anställd):
+    def __init__(self, namn):
+        super().__init__(namn)
+    
+    def beskrivning(self):
+        return super().beskrivning()
+    
+r = Rektor
+l = Lärare
